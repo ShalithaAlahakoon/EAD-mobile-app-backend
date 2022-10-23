cd727064fa6a84d37b049515e8f551100bdd044c
SandunDharmadasa <sandundharmaasa0621@gmail.com>
2022-10-23 11:27:20 +0530

    Backend of Station Owner

--- routers/router.js
+++ routers/router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/loginController.js');
+const fuelCntroller = require('../controllers/fuelController');
 
 
 //get logins
 router.route('/logins')
     .get(loginController.getAllLogins)
     .post(loginController.createLogin);
+
+//fuelRoutes
+router.route('/fuel')
+    .post(fuelCntroller.addFuels);
 
 
 
